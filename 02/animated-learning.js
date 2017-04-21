574789aac4aca0ba0c10c17f13b9bbb7db8c3bf1
Micah Stubbs <micah.stubbs@gmail.com>
2017-04-21 10:48:47 -0500

    manually make code style more friendly for a native js eye

--- 02/animated-learning.js
+++ 02/animated-learning.js
@@ -3,7 +3,9 @@
 function animatedLearning() {
   const svg = d3.select('body').select('svg');
 
-  const x = d3.scaleLinear().range([200, 500]).domain([0, 1]);
+  const x = d3.scaleLinear()
+    .range([200, 500])
+    .domain([0, 1]);
 
   const learningRate = 0.2;
 
@@ -15,12 +17,22 @@ function animatedLearning() {
 
   const As = [];
   for (let i = 0; i < nAs; i += 1) {
-    As.push({ id: i, latent_value: Math.random(), current_position: 0.5, next_position: 0.5 });
+    As.push({ 
+      id: i,
+      latentValue: Math.random(),
+      currentPosition: 0.5, 
+      nextPosition: 0.5
+    });
   }
 
   const Bs = [];
   for (let i = 0; i < nBs; i += 1) {
-    Bs.push({ id: i, latent_value: Math.random(), current_position: 0.5, next_position: 0.5 });
+    Bs.push({
+      id: i,
+      latentValue: Math.random(),
+      currentPosition: 0.5,
+      nextPosition: 0.5
+    });
   }
 
   // construct circles
@@ -51,10 +63,18 @@ function animatedLearning() {
       const aId = Math.floor(Math.random() * nAs);
 
       // pair selection a stochastic function of distance from respective current positions
-      const weights = Bs.map(d => d.current_position - As[aId].current_position);
+      const weights = Bs.map(d => d.currentPosition - As[aId].currentPosition);
+
       let cumWeights = [];
-      weights.reduce((a, b, i) => { cumWeights[i] = { v: a + b, id: i }; return a + b; }, 0);
+      weights.reduce((a, b, i) => { 
+        cumWeights[i] = { 
+          v: a + b, 
+          id: i 
+        }; 
+        return a + b; 
+      }, 0);
       cumWeights = cumWeights.sort((a, b) => a.v > b.v);
+
       let bId = Math.floor(Math.random() * nBs);
       if (cumWeights[cumWeights.length - 1].v !== 0) {
         const selRandom = Math.random() * cumWeights[cumWeights.length - 1].v;
@@ -67,21 +87,21 @@ function animatedLearning() {
       pairs.push({ aId, bId });
 
       // big = 1, small = -1
-      const feedback = -1 + (2 * (As[aId].latent_value > Bs[bId].latent_value));
+      const feedback = -1 + (2 * (As[aId].latentValue > Bs[bId].latentValue));
 
       // use feedback if it contradicts current
-      if ((feedback === -1) && (As[aId].current_position <= Bs[bId].current_position)) {
-        As[aId].next_position = As[aId].current_position + (Math.random() * learningRate);
-        Bs[bId].next_position = Bs[bId].current_position - (Math.random() * learningRate);
+      if ((feedback === -1) && (As[aId].currentPosition <= Bs[bId].currentPosition)) {
+        As[aId].nextPosition = As[aId].currentPosition + (Math.random() * learningRate);
+        Bs[bId].nextPosition = Bs[bId].currentPosition - (Math.random() * learningRate);
       }
 
-      if ((feedback === 1) && (As[aId].current_position >= Bs[bId].current_position)) {
-        As[aId].next_position = As[aId].current_position - (Math.random() * learningRate);
-        Bs[bId].next_position = Bs[bId].current_position + (Math.random() * learningRate);
+      if ((feedback === 1) && (As[aId].currentPosition >= Bs[bId].currentPosition)) {
+        As[aId].nextPosition = As[aId].currentPosition - (Math.random() * learningRate);
+        Bs[bId].nextPosition = Bs[bId].currentPosition + (Math.random() * learningRate);
       }
 
-      As[aId].next_position = Math.min(1, Math.max(0, As[aId].next_position));
-      Bs[bId].next_position = Math.min(1, Math.max(0, Bs[bId].next_position));
+      As[aId].nextPosition = Math.min(1, Math.max(0, As[aId].nextPosition));
+      Bs[bId].nextPosition = Math.min(1, Math.max(0, Bs[bId].nextPosition));
     }
 
     // *** SVG ANIMATION ***
@@ -93,39 +113,47 @@ function animatedLearning() {
 
     svg.selectAll('.pair').remove();
 
-    svg.selectAll('.pair').data(pairs).enter().append('line')
-      .attr('class', 'pair')
-      .attr('y1', 230)
-      .attr('y2', 270)
-      .style('stroke', '#000')
-      .style('stroke-width', 0.25)
-      .style('fill', 'none')
-      .attr('x1', d => x(As[d.aId].current_position))
-      .attr('x2', d => x(Bs[d.bId].current_position))
-      .transition().delay(delay).duration(move)
-        .attr('x1', d => x(As[d.aId].next_position))
-        .attr('x2', d => x(Bs[d.bId].next_position));
+    svg.selectAll('.pair')
+      .data(pairs)
+      .enter().append('line')
+        .attr('class', 'pair')
+        .attr('y1', 230)
+        .attr('y2', 270)
+        .style('stroke', '#000')
+        .style('stroke-width', 0.25)
+        .style('fill', 'none')
+        .attr('x1', d => x(As[d.aId].currentPosition))
+        .attr('x2', d => x(Bs[d.bId].currentPosition))
+        .transition()
+          .delay(delay)
+          .duration(move)
+          .attr('x1', d => x(As[d.aId].nextPosition))
+          .attr('x2', d => x(Bs[d.bId].nextPosition));
 
     // animate circles
 
     svg.selectAll('.A')
-      .transition().delay(delay).duration(move)
-        .attr('cx', d => x(d.next_position));
+      .transition()
+        .delay(delay)
+        .duration(move)
+        .attr('cx', d => x(d.nextPosition));
 
     svg.selectAll('.B')
-      .transition().delay(delay).duration(move)
-        .attr('cx', d => x(d.next_position));
+      .transition()
+        .delay(delay)
+        .duration(move)
+        .attr('cx', d => x(d.nextPosition));
 
     // *** end of svg animation code ***
 
     // prep next timestep
 
     As.forEach((d) => {
-      d.current_position = d.next_position;
+      d.currentPosition = d.nextPosition;
     });
 
     Bs.forEach((d) => {
-      d.current_position = d.next_position;
+      d.currentPosition = d.nextPosition;
     });
   }, 1200);
 }
