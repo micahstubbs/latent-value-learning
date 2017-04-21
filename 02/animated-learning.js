10eb064e7a79ae232698cf9c891d36750660cf0a
Micah Stubbs <micah.stubbs@gmail.com>
2017-04-21 10:29:26 -0500

    lebab.sh

--- 02/animated-learning.js
+++ 02/animated-learning.js
@@ -1,38 +1,38 @@
 
 function animated_learning() {
 
-  var svg = d3.select("body").select("svg")
+  const svg = d3.select("body").select("svg");
 
-  var x = d3.scaleLinear().range([200,500]).domain([0,1])
+  const x = d3.scaleLinear().range([200,500]).domain([0,1]);
 
-  var learning_rate = 0.2
+  const learning_rate = 0.2;
 
-  var n_As = 10
-  var n_Bs = 10
-  var n_pairs = 8
+  const n_As = 10;
+  const n_Bs = 10;
+  const n_pairs = 8;
 
   // create elements with latent values and initial positions
 
-  var As = []
+  const As = [];
   for (var i=0; i < n_As; i++){
     As.push({id: i, latent_value: Math.random(), current_position: 0.5, next_position: 0.5})
   }
 
-  var Bs = []
+  const Bs = [];
   for (var i=0; i < n_Bs; i++){
     Bs.push({id: i, latent_value: Math.random(), current_position: 0.5, next_position: 0.5})
   }
 
   // construct circles
 
-  svg.selectAll(".A").data(As, function(d){ return d.id; })
+  svg.selectAll(".A").data(As, d => d.id)
     .enter().append("circle")
       .attr("class", "A A-color")
       .attr("cx", 350)
       .attr("cy", 230)
       .attr("r", 3)
 
-  svg.selectAll(".B").data(Bs, function(d){ return d.id; })
+  svg.selectAll(".B").data(Bs, d => d.id)
     .enter().append("circle")
       .attr("class", "B B-color")
       .attr("cx", 350)
@@ -42,33 +42,33 @@ function animated_learning() {
 
   // simulation / animation loop
 
-  d3.interval(function(){
+  d3.interval(() => {
 
     // *** SIMULATION ***
 
-    var pairs = []
+    const pairs = [];
 
-    for (var i=0; i < n_pairs; i++) {
-      var A_id = Math.floor(Math.random()*n_As)
+    for (let i=0; i < n_pairs; i++) {
+      const A_id = Math.floor(Math.random()*n_As);
 
       // pair selection a stochastic function of distance from respective current positions
-      var weights = Bs.map(function(d){ return d.current_position - As[A_id].current_position; })
-      var cum_weights = []
-      weights.reduce(function(a,b,i) { cum_weights[i] = {v: a+b, id:i}; return a + b; },0)
-      cum_weights = cum_weights.sort(function(a,b){ return a.v > b.v; })
-      var B_id = Math.floor(Math.random()*n_Bs)
+      const weights = Bs.map(d => d.current_position - As[A_id].current_position);
+      let cum_weights = [];
+      weights.reduce((a, b, i) => { cum_weights[i] = {v: a+b, id:i}; return a + b; },0)
+      cum_weights = cum_weights.sort((a, b) => a.v > b.v)
+      let B_id = Math.floor(Math.random()*n_Bs);
       if (cum_weights[cum_weights.length - 1].v != 0) {
-        var sel_random = Math.random() * cum_weights[cum_weights.length - 1].v
-        var sel = cum_weights.find(function(d){ return d.v >= sel_random; })
+        const sel_random = Math.random() * cum_weights[cum_weights.length - 1].v;
+        const sel = cum_weights.find(d => d.v >= sel_random);
         if (!(sel == null)) {
           B_id = sel.id
         }
       }
 
-      pairs.push({A_id: A_id, B_id: B_id})
+      pairs.push({A_id, B_id})
 
       // big = 1, small = -1
-      var feedback = -1 + 2 * (As[A_id].latent_value > Bs[B_id].latent_value)
+      const feedback = -1 + 2 * (As[A_id].latent_value > Bs[B_id].latent_value);
 
       // use feedback if it contradicts current
       if ((feedback == -1) && (As[A_id].current_position <= Bs[B_id].current_position)) {
@@ -88,8 +88,8 @@ function animated_learning() {
 
     // *** SVG ANIMATION ***
 
-    var delay = 400
-    var move = 500
+    const delay = 400;
+    const move = 500;
 
     // draw and animate pair lines
 
@@ -102,31 +102,31 @@ function animated_learning() {
       .style("stroke", "#000")
       .style("stroke-width", 0.25)
       .style("fill", "none")
-      .attr("x1", function(d){ return x(As[d.A_id].current_position); })
-      .attr("x2", function(d){ return x(Bs[d.B_id].current_position); })
+      .attr("x1", d => x(As[d.A_id].current_position))
+      .attr("x2", d => x(Bs[d.B_id].current_position))
       .transition().delay(delay).duration(move)
-        .attr("x1", function(d){ return x(As[d.A_id].next_position); })
-        .attr("x2", function(d){ return x(Bs[d.B_id].next_position); })
+        .attr("x1", d => x(As[d.A_id].next_position))
+        .attr("x2", d => x(Bs[d.B_id].next_position))
 
     // animate circles
 
     svg.selectAll(".A")
       .transition().delay(delay).duration(move)
-        .attr("cx", function(d){ return x(d.next_position); })
+        .attr("cx", d => x(d.next_position))
 
     svg.selectAll(".B")
       .transition().delay(delay).duration(move)
-        .attr("cx", function(d){ return x(d.next_position); })
+        .attr("cx", d => x(d.next_position))
 
     // *** end of svg animation code ***
 
     // prep next timestep
 
-    As.forEach(function(d){
+    As.forEach(d => {
       d.current_position = d.next_position
     })
 
-    Bs.forEach(function(d){
+    Bs.forEach(d => {
       d.current_position = d.next_position
     })
 
---
+++ 02/lebab.sh
@@ -0,0 +1,12 @@
+# safe
+lebab --replace animated-learning.js --transform arrow
+lebab --replace animated-learning.js --transform for-of
+lebab --replace animated-learning.js --transform for-each
+lebab --replace animated-learning.js --transform arg-rest
+lebab --replace animated-learning.js --transform arg-spread
+lebab --replace animated-learning.js --transform obj-method
+lebab --replace animated-learning.js --transform obj-shorthand
+lebab --replace animated-learning.js --transform multi-var
+# unsafe
+lebab --replace animated-learning.js --transform let
+lebab --replace animated-learning.js --transform template
